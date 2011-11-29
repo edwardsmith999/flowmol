a9e8b74529550f7b3eb134f7e46210671be8835c
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2011-11-29 12:00:12 +0000

    Commented the visualisation scripts.
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@77 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- MD_dCSE/src_code/results/vis/read_header.py
+++ MD_dCSE/src_code/results/vis/read_header.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python
-f = open('simulation_header','r')
-headerdata = f.readlines()
-for i in range(len(headerdata)):
-	headerdata[i] = headerdata[i].split(';')
-	for j in range(len(headerdata[i])):
-		headerdata[i][j] = headerdata[i][j].strip().replace('(','').replace(')','')
-	vars()[headerdata[i][1]] = headerdata[i][2]
-f.close()
+#########################################################################################
+# read_header.py
+# author: David Trevelyan
+#
+# This script is designed for importing into other python routines that require
+# information from the file 'simulation_header'
+#
+# ---------------------------------------------------------------------------------------
 
-f = open('simulation_progress','r')
-Nsteps = f.read().strip()
-f.close()
+f = open('simulation_header','r')						# Create ascii file object
+headerdata = f.readlines()								# Read all lines into list
+for i in range(len(headerdata)):						# Loop through all elements in list (i.e. lines in simulation_header) 
+	headerdata[i] = headerdata[i].split(';')			# Split list elements into sub-lists according to ; character in simulation_header
+	for j in range(len(headerdata[i])):					# Loop through all elements in sub-list
+		headerdata[i][j] = headerdata[i][j].strip().replace('(','').replace(')','') # Strip white space and remove brackets (can't create arrays using vars)
+	vars()[headerdata[i][1]] = headerdata[i][2]			# Create variables with name headerdata[i][1] and value headerdata[i][2]
+f.close()												# Close file object
+
+f = open('simulation_progress','r')						# Creat ascii file object
+Nsteps = f.read().strip()								# Read number of timesteps 
+f.close()												# Close file object
