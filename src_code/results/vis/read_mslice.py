a72fbf8f4dbcfa0bbdc1071147d1d763108fb737
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2011-11-29 12:00:12 +0000

    Commented the visualisation scripts.
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@77 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/results/vis/read_mslice.py
+++ src_code/results/vis/read_mslice.py
@@ -1,10 +1,20 @@
 #! /usr/bin/env python
+######################################################################################
+# read_mslice.py
+# author: David Trevelyan
+#
+# read_mslice.py reads the binary data from the file 'mslice' and stores it in 
+# a numpy array. This can then be used as part of read_vslice to plot the average
+# velocity profile from a simulation.
+# 
+# ------------------------------------------------------------------------------------
 from numpy import *
 from array import array
 import read_header
 import math
 import os
 
+# Retrieve important variables from simulation_header with read_header.py
 Nsteps 	  		= int(read_header.Nsteps)
 tplot  	  		= int(read_header.tplot)
 Nmass_ave 		= int(read_header.Nmass_ave)
@@ -13,8 +23,8 @@ globalnbins 	= [int(read_header.globalnbins1),int(read_header.globalnbins2),int(
 Nmass_records 	= int(math.floor(Nsteps/(tplot*Nmass_ave)))
 nbins			= int(globalnbins[m_outflag-1])
 
-f = open('mslice','rb')
-mslice = array('i')
-mslice.fromfile(f,nbins*Nmass_records)
-mslice = reshape(mslice,(Nmass_records,nbins))
-f.close()
+f = open('mslice','rb')								# Create binary file object
+mslice = array('i')									# Initialise array of integers
+mslice.fromfile(f,nbins*Nmass_records)				# Read entire binary file into array
+mslice = reshape(mslice,(Nmass_records,nbins))		# Reshape array into 3 dimensions
+f.close()											# Close file object
--- src_code/results/vis/read_vslice.py
+++ src_code/results/vis/read_vslice.py
@@ -1,4 +1,26 @@
 #! /usr/bin/env python
+#########################################################################################
+# read_vslice.py - PRINTS THE VELOCITY PROFILE ACROSS A NORMALISED DOMAIN
+# author: David Trevelyan
+#
+# read_vslice.py reads all the 'snapshot' velocity profiles from the file 'vslice' and
+# calculates the mean profile from the final HALF of all the snapshots. This can be
+# easily changed by adjusting how many values in the vslice array (created in this
+# script) are passed to the function 'mean(values)' when preparing the output print
+# string. The output print string is designed to be easily piped and is formatted 
+# as a normalised position across the domain followed by the average velocity for the
+# bin centered at the corresponding 'unnormalised' position. read_vslice is designed
+# to be called from the results folder.
+# 
+# EXAMPLE (if v_outflag=2 and shear_direction=1):
+#	
+#		./vis/read_vslice.py > vprofile	
+#		gnuplot
+#	  > set xlabel "u_x"
+#	  > set ylabel "y/Y"
+#	  > plot [][0:1] vprofile u 2:1 w p
+#
+# ---------------------------------------------------------------------------------------
 from numpy import *
 from array import array
 import math
@@ -24,7 +46,7 @@ mslice = read_mslice.mslice
 f = open('vslice','rb')								# Create file object f to read from vslice
 #dble_filesize = int(os.path.getsize('vslice')/8)	# Calculate number of double precision records
 vslice = array('d')									# Create vslice array in which to store data
-vslice.fromfile(f,Nvel_records*nd*nbins)					# Store vslice data
+vslice.fromfile(f,Nvel_records*nd*nbins)			# Store vslice data
 vslice = reshape(vslice,(Nvel_records,nd,nbins)) 	# Reshape array into number of records, dimensions and bins
 f.close()
 
