e9b652ebb1e496e0ea2869b46bc081e3704878d0
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2011-12-16 18:03:14 +0000

    Extensive formatting changes throughout coupler - all spaces replaced with tabs of size 4. Some logical statements replaced with word equivalents (i.e. == .eq. etc) as in other codes. Commenting throughout the code with all subroutines given descriptions. md.f90 cleaned and subroutines moved to md socket to be consistant with CFD (dummy also added as with CFD). Socket init and main consoldated into single file
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@90 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/external_forces.f90
+++ src_code/external_forces.f90
@@ -26,54 +26,25 @@ end module module_external_forces
 !Apply force to prevent molecules leaving domain using form suggested by Nie, Chen and
 !Robbins (2004)
 
-subroutine simulation_apply_constraint_forces
-use module_external_forces
-use coupler, only : coupler_md_constrain_forces
-implicit none
+subroutine simulation_apply_boundary_forces
+	use module_external_forces
+	use coupler, only : coupler_md_boundary_forces
+	implicit none
 
-! call the coupler version.
-! This is needed because the coupler has the information about the continuum grid
-
+	! call the coupler version.
+	! This is needed because the coupler has the information about the continuum grid
 	pressure = 2.5d0
 
-	call coupler_md_constrain_forces(np,pressure,r,a)
-
-!!$	integer	:: n
-!!$	double precision :: delta_y
-!!$	double precision :: y, y0, y1, y2, y3
-!!$
-!!$	if (jblock .eq. npy) then
-!!$
-!!$		delta_y = globaldomain(2)/6.d0
-!!$	
-!!$		y0 = 0.d0
-!!$		y1 = y0 + delta_y
-!!$		y2 = y1 + delta_y
-!!$		y3 = globaldomain(2)/2.d0
-!!$	
-!!$		do n = 1, np
-!!$			if (r(n,2) .gt. y2) then
-!!$		
-!!$				y = r(n,2)
-!!$				!Initial pressure is -ve - this line prevents problems
-!!$				if (pressure .lt. 0.d0) then
-!!$					a(n,2)= a(n,2) - (y-y2)/(1-(y-y2)/(y3-y2))
-!!$				else
-!!$					a(n,2)= a(n,2) - (y-y2)/(1-(y-y2)/(y3-y2))*pressure
-!!$				endif
-!!$			endif
-!!$		enddo
-!!$
-!!$	endif
+	call coupler_md_boundary_forces(np,pressure,r,a)
 	
-end subroutine simulation_apply_constraint_forces
+end subroutine simulation_apply_boundary_forces
 
 !--------------------------------------------------------------------------------------
 !Apply force to give linear profile
 
 subroutine simulation_apply_linear_forces
-use module_external_forces
-implicit none
+	use module_external_forces
+	implicit none
 
 	!double precision :: y, y0, y1, y2, y3
 	integer         			:: n, molno, ixyz
@@ -145,8 +116,8 @@ end subroutine simulation_apply_linear_forces
 !Apply a force with the same value to all molecules
 
 subroutine simulation_apply_constant_force(ixyz,F_const)
-use module_external_forces
-implicit none
+	use module_external_forces
+	implicit none
 
 	integer         			:: ixyz
 	double precision 			:: F_const
---
+++ src_code/md_coupler_socket.f90
@@ -0,0 +1,130 @@
+!=============================================================================
+!				   
+! Routine which interface with the coupler to the CFD code
+! Corresponding empty shell dummy routine used for uncoupled calculation
+!
+!  Lucian Anton, November 2011
+!
+!-----------------------------------------------------------------------------
+
+
+module md_coupler_socket
+	implicit none
+
+contains
+
+!=============================================================================
+! Setup initial times based on coupled calculation
+!-----------------------------------------------------------------------------
+subroutine socket_coupler_init
+	use coupler
+	use computational_constants_MD, only : npx,npy,npz,delta_t,elapsedtime,Nsteps,initialstep, delta_t
+	use messenger, only	      :  myid, icoord
+	implicit none
+
+ 	integer nsteps_cfd
+	real(kind(0.d0)) :: delta_t_CFD
+
+	if (.not. coupler_is_active) return
+
+	! if coupled calculation prepare exchange layout
+	call coupler_md_init(npx,npy,npz,icoord,delta_t)
+
+	! fix NSTEPS for the coupled case
+	delta_t_CFD = coupler_md_get_dt_cfd()
+	nsteps_cfd  = coupler_get_nsteps()
+
+	Nsteps = initialstep + nsteps_cfd * int(delta_t_cfd/delta_t)
+	elapsedtime = elapsedtime + nsteps_cfd * delta_t_CFD
+
+	if (myid .eq. 0) then 
+		write(*,'(4(a,/),a,I7,/a,/a,E10.4,a,/,a,/a)') &
+				"*********************************************************************", 		&
+ 				"WARNING - this is a coupled run which resets the number	      ", 		&
+				" of extrasteps to:						   ", 		&
+				"								     ", 		&
+				" nstep_cfd*(delta_t_cdf/delta_t_md) = ",nsteps_cfd*int(delta_t_cfd/delta_t), 	&
+				"								     ", 		& 
+				" The elapsed time was changed accordingly to: ", elapsedtime, " s    ", 		&
+				" The value of NSTEPS parameter form input file was discarded.	", 		&
+				"*********************************************************************"   
+	endif 
+
+end subroutine socket_coupler_init
+
+!=============================================================================
+!Apply coupling forces so MD => CFD
+!-----------------------------------------------------------------------------
+subroutine socket_coupler_apply_continuum_forces(iter)
+	use computational_constants_MD, only : delta_t
+	use physical_constants_MD, only : np
+	use arrays_MD, only :r,v,a
+	use coupler
+	implicit none
+	
+	integer, intent(in) :: iter
+
+	integer :: iter_average, Naverage
+	real(kind(0.d0)) :: delta_t_CFD
+	logical, save :: first_time=.true.
+	save Naverage
+
+	if (.not. coupler_is_active) return 
+
+	if (first_time) then
+		first_time = .false.
+		Naverage = int(coupler_md_get_dt_cfd()/delta_t)
+	endif
+	iter_average = mod(iter-1, Naverage)+1
+
+	call coupler_md_apply_continuum_forces(np,r,v,a,iter_average)	!Apply force based on Nie,Chen an Robbins coupling
+	
+end subroutine socket_coupler_apply_continuum_forces
+
+!=============================================================================
+! Calculate averages of MD to pass to CFD
+!-----------------------------------------------------------------------------
+subroutine socket_coupler_average(iter)
+	use computational_constants_MD, only : initialstep, delta_t
+	use physical_constants_MD, only : np
+	use arrays_MD, only :r,v
+	use coupler
+	implicit none
+
+	integer, intent(in) :: iter
+	
+	integer :: iter_cfd, iter_average, Naverage, save_period, average_period
+	logical, save :: first_time=.true.
+	save save_period, average_period, Naverage
+
+	if (.not. coupler_is_active) return
+
+    if (first_time) then 
+	    first_time     = .false.
+	    save_period 	= coupler_get_save_period()    ! period to save uc data (for debugging, testing)
+	    average_period = coupler_get_average_period() ! collection interval in the average cycle
+	    Naverage = int(coupler_md_get_dt_cfd() /delta_t)	   ! number of steps in MD average cycle
+    endif
+
+    iter_average = mod(iter-1, Naverage)+1	! current step
+    
+    iter_cfd     = (iter-initialstep)/Naverage +1 ! CFD corresponding step
+
+    if ( mod(iter_average,average_period) .eq. 0 ) then
+	    call coupler_md_boundary_cell_average(np,r,v,send_data=.false.) ! accumlate velocities
+	    if ( mod(iter_cfd,save_period) .eq. 0) then
+		    call coupler_uc_average_test(np,r,v,lwrite=.false.)
+	    endif
+    endif
+
+	! Send accumulated results to CFD at the end of average cycle 
+	if (iter_average .eq. Naverage) then 
+	    call coupler_md_boundary_cell_average(np,r,v,send_data=.true.)
+	    if (mod(iter_cfd,save_period) .eq. 0 ) then
+		    call coupler_uc_average_test(np,r,v,lwrite=.true.)
+	    endif
+	endif
+	    	
+end subroutine socket_coupler_average
+
+end module md_coupler_socket
