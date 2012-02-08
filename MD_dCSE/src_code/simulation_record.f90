0f5b301fa22db3e1ec7f14f03cdf2da682f63c6c
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-02-08 17:36:33 +0000

    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@123 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- MD_dCSE/src_code/simulation_record.f90
+++ MD_dCSE/src_code/simulation_record.f90
@@ -2111,10 +2111,8 @@ implicit none
 
 		!Stress acting on face over volume
 		if (eflux_outflag .ne. 0) then
-			!velvect(:) = v(molnoi,:) 
-!TEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMP
-			velvect(:) = v(molnoi,:) + 0.5d0*delta_t*aold(molnoi,:)
-!TEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMPTEMP
+			velvect(:) = v(molnoi,:) 
+
 			Pxyvface(cbin(1),cbin(2),cbin(3),1) = Pxyvface(cbin(1),cbin(2),cbin(3),1) + dot_product(fij,velvect)*dble(onfacexb)
 			Pxyvface(cbin(1),cbin(2),cbin(3),2) = Pxyvface(cbin(1),cbin(2),cbin(3),2) + dot_product(fij,velvect)*dble(onfaceyb)
 			Pxyvface(cbin(1),cbin(2),cbin(3),3) = Pxyvface(cbin(1),cbin(2),cbin(3),3) + dot_product(fij,velvect)*dble(onfacezb)
