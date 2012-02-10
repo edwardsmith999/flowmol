2571deac14c18a7e26a8e1791ed853963fc21a3d
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-02-10 14:22:46 +0000

    Added tag_move system to simulation_move_particles_lfv. Updated default.in to not crash the code!
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@125 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- MD_dCSE/src_code/simulation_move_particles_lfv.f90
+++ MD_dCSE/src_code/simulation_move_particles_lfv.f90
@@ -69,7 +69,10 @@ implicit none
 	
 	case(nvt_pwa_NH)
 		stop 'pwa_NH not available for leap-frog Verlet.'
-				
+
+	case(tag_move)
+		call simulation_move_particles_lfv_tag
+						
 	case default
 		stop 'Unrecognised move option in simulation_move_particles_lfv.f90'
 
@@ -161,6 +164,118 @@ contains
 		deallocate(v_avg)
 
 	end subroutine evaluate_U_PUT
+
+	!---------------------------------------------------------------------
+	!Tag move routine
+	subroutine simulation_move_particles_lfv_tag
+		implicit none
+				
+		integer :: maxtag, n, thermostatnp, slicebin
+		integer, dimension(:), allocatable 	:: m_slice
+		double precision :: dzeta_dt, pec_v2sum, v2sum
+		double precision :: ascale, bscale, alpha, beta
+		double precision, dimension(nd)	:: vel
+		logical :: PUT
+
+		if (all(tag.eq.8)) then		
+			PUT = .true.
+			call evaluate_U_PUT										! Only works for serial code.
+			pec_v2sum = 0.d0
+		else 
+			PUT = .false.
+		end if
+
+		!Check if any molecules are thermostatted and calculate appropriate coefficients
+		maxtag = maxval(tag)
+		call globalMaxInt(maxtag)
+		if (maxtag .ge. 4) then
+
+			v2sum = 0.d0    									  	! Reset total v2sum
+			thermostatnp = 0										! Reset count of thermostatted mols
+			do n = 1, np   											! Loop all molecules
+				if (tag(n) .lt. 4) cycle							! Only include thermostatted molecules - DO YOU WANT THIS LINE UNCOMMENTED?
+				if (PUT) then										! PUT: If using PUT find peculiar v2sum
+					vel(:) = v(n,:) - U(n,:) - 0.5d0*a(n,:)*delta_t			! PUT: Find peculiar velocity
+					pec_v2sum = pec_v2sum + dot_product(vel,vel)						! PUT: Sum peculiar velocities squared
+				else
+					vel(:) = v(n,:) - 0.5d0*a(n,:)*delta_t	
+					v2sum = v2sum + dot_product(vel,vel)
+				end if
+				thermostatnp = thermostatnp + 1
+			enddo
+
+			!Obtain global sums for all parameters
+			call globalSumInt(thermostatnp)
+			call globalSum(v2sum)	
+
+			Q        = thermostatnp * delta_t
+			dzeta_dt = (v2sum - (nd*thermostatnp + 1)*inputtemperature) / Q
+			if (PUT) dzeta_dt = (pec_v2sum - (nd*thermostatnp + 1)*inputtemperature) / Q
+			zeta 	 = zeta + delta_t*dzeta_dt
+			bscale	 = 1.0/(1.0+0.5*delta_t*zeta)
+			ascale	 = (1-0.5*delta_t*zeta)*bscale
+
+		endif
+
+		!Step through each particle n
+		do n = 1,np        
+			select case (tag(n))
+			case (0)
+				!Leapfrog mean velocity calculated here at v(t+0.5delta_t) = v(t-0.5delta_t) + a*delta_t 
+				!Leapfrog mean position calculated here at r(t+delta_t) = r(t) + v(t+0.5delta_t)*delta_t
+				v(n,:) = v(n,:) + delta_t * a(n,:) 	!Velocity calculated from acceleration
+				r(n,:) = r(n,:) + delta_t * v(n,:)	!Position calculated from velocity
+			case (1)
+				!Fixed Molecules - no movement r(n+1) = r(n)
+			case (2)
+				!Fixed with constant sliding speed
+				r(n,:) = r(n,:) + delta_t*slidev(n,:)	!Position calculated from velocity
+			case (3)
+				!Tethered molecules
+				call tether_force(n)
+				v(n,:) = v(n,:) + delta_t * a(n,:) 	!Velocity calculated from acceleration
+				r(n,:) = r(n,:) + delta_t * v(n,:)	!Position calculated from velocity
+			case (4)
+				!Nose Hoover Thermostatted Molecule
+				v(n,1) = v(n,1)*ascale + a(n,1)*delta_t*bscale
+				r(n,1) = r(n,1)    +     v(n,1)*delta_t			
+				v(n,2) = v(n,2)*ascale + a(n,2)*delta_t*bscale
+				r(n,2) = r(n,2)    + 	 v(n,2)*delta_t				
+				v(n,3) = v(n,3)*ascale + a(n,3)*delta_t*bscale
+				r(n,3) = r(n,3)    +     v(n,3)*delta_t	
+			case (5)
+				!Thermostatted Tethered molecules unfixed with no sliding velocity
+				call tether_force(n)
+				v(n,1) = v(n,1)*ascale + a(n,1)*delta_t*bscale
+				r(n,1) = r(n,1)    +     v(n,1)*delta_t			
+				v(n,2) = v(n,2)*ascale + a(n,2)*delta_t*bscale
+				r(n,2) = r(n,2)    + 	 v(n,2)*delta_t				
+				v(n,3) = v(n,3)*ascale + a(n,3)*delta_t*bscale
+				r(n,3) = r(n,3)    +     v(n,3)*delta_t	
+			case (6)
+				!Tethered molecules with sliding velocity
+				call tether_force(n)
+				v(n,:) = v(n,:) + delta_t * a(n,:) 							!Velocity calculated from acceleration
+				r(n,:) = r(n,:) + delta_t * v(n,:) + delta_t*slidev(n,:)	!Position calculated from velocity+slidevel
+			case (7)
+				!Thermostatted Tethered molecules unfixed with sliding velocity
+				call tether_force(n)
+				v(n,1) = v(n,1)*ascale + a(n,1)*delta_t*bscale
+				r(n,1) = r(n,1)    +     v(n,1)*delta_t	+ slidev(n,1)*delta_t		
+				v(n,2) = v(n,2)*ascale + a(n,2)*delta_t*bscale
+				r(n,2) = r(n,2)    + 	 v(n,2)*delta_t	+ slidev(n,2)*delta_t			
+				v(n,3) = v(n,3)*ascale + a(n,3)*delta_t*bscale
+				r(n,3) = r(n,3)    +     v(n,3)*delta_t	+ slidev(n,3)*delta_t
+			case (8)
+				!Profile unbiased thermostat (Nose-Hoover)
+	        	v(n,:) = v(n,:)*ascale + a(n,:)*delta_t*bscale + zeta*U(n,:)*delta_t*bscale
+				r(n,:) = r(n,:)        + v(n,:)*delta_t			
+			case default
+				stop "Invalid molecular Tag"
+			end select
+		enddo
+		
+	end subroutine simulation_move_particles_lfv_tag
 
 end subroutine simulation_move_particles_lfv
 
