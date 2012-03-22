a2c9633f959e6ef5f2338311f66a56a1ffc25d02
luciana <luciana@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-03-22 14:00:32 +0000

    Corrections and reorganisation of continuum and constrain force calculation
    constrain and continuum force are computed in MD (not in coupler as before)
    using MD computational cells for efficiency ( to be measured!)
    
    some small cosmetic changes/comments made in coupler
    tests passed
    
    more refinment is needed for continuum force:
    
    This version neglects the molecules that are outside the local MD
    domain. Statistically their impact should be small.
    Future version should communicate to the corresponding rank the sums of velocities and
    acceleration for molecules outside local MD domain.
    
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@162 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/md_coupler_socket.f90
+++ src_code/md_coupler_socket.f90
@@ -17,6 +17,14 @@ module md_coupler_socket
 
 #if USE_COUPLER 
 
+! type used in applying continuum force
+	type cfd_box_sum
+		integer np
+		real(kind=kind(0.d0))  v(3)
+		real(kind=kind(0.d0))  a(3)
+	end type cfd_box_sum
+
+
 contains
 
 !=============================================================================
@@ -62,10 +70,10 @@ end subroutine socket_coupler_init
 !Apply coupling forces so MD => CFD
 !-----------------------------------------------------------------------------
 subroutine socket_coupler_apply_continuum_forces(iter)
+	use coupler
 	use computational_constants_MD, only : delta_t
 	use physical_constants_MD, only : np
 	use arrays_MD, only :r,v,a
-	use coupler
 	implicit none
 	
 	integer, intent(in) :: iter
@@ -75,15 +83,18 @@ subroutine socket_coupler_apply_continuum_forces(iter)
 	logical, save :: first_time=.true.
 	save Naverage
 
-	if (.not. coupler_is_active) return 
-
 	if (first_time) then
 		first_time = .false.
 		Naverage = int(coupler_md_get_dt_cfd()/delta_t)
 	endif
 	iter_average = mod(iter-1, Naverage)+1
 
-	call coupler_md_apply_continuum_forces(np,r,v,a,iter_average)	!Apply force based on Nie,Chen an Robbins coupling
+!	coupler library version ( deprecated )
+!	call coupler_md_apply_continuum_forces(np,r,v,a,iter_average)	!Apply force based on Nie,Chen an Robbins coupling
+
+!  use this module version
+	call apply_continuum_forces(iter_average)
+
 	
 end subroutine socket_coupler_apply_continuum_forces
 
@@ -134,6 +145,229 @@ subroutine socket_coupler_average(iter)
 	endif
 	    	
 end subroutine socket_coupler_average
+
+
+!=============================================================================
+! Apply force from Nie et al (2004) paper to fix molecular velocity to
+! continuum value inside the overlap region. 
+!-----------------------------------------------------------------------------
+subroutine apply_continuum_forces(iter)
+	use coupler
+	use computational_constants_MD, only : delta_t, nh, ncells, cellsidelength, halfdomain
+	use linked_list
+	use arrays_MD, only : r, v, a
+	implicit none
+
+	integer, intent(in) :: iter ! iteration step, it assumes that each MD average
+								! starts from iter = 1
+
+	type(cfd_box_sum),allocatable :: box_average(:,:,:)
+	integer i, j, k, js, je, ib, jb, kb, nib, njb, nkb, ip,m, np_overlap
+	integer, allocatable :: list(:,:)
+	real(kind=kind(0.d0))  inv_dtCFD,ymin,ymax,xmin,xmax,zmin,zmax,dx_cfd,dz_cfd
+	real(kind(0.d0)), allocatable :: vel_cfd(:,:,:,:,:)
+	logical overlap
+	type(node), pointer :: current => null()
+	integer, save :: ncalls = 0
+
+	! run through the particle, check if they are in the overlap region
+	! find the CFD box to which the particle belongs		  
+	! attention to the particle that have left the domain boundaries 
+
+	! check if this MD domain overlap with the region in which continuum
+	! force is applied
+	call coupler_md_get(overlap_with_continuum_force=overlap)
+
+	if (overlap) then 
+
+		! get cfd cell sizes
+		call coupler_md_get(ymin_continuum_force=ymin, ymax_continuum_force=ymax, xmin_cfd_grid=xmin, &
+			xmax_cfd_grid=xmax, zmin_cfd_grid=zmin, zmax_cfd_grid=zmax, dx_cfd=dx_cfd, dz_cfd=dz_cfd)
+
+		! number of CFD cells in each direction
+		nib = nint((xmax - xmin)/ dx_cfd)
+		njb = 1
+		nkb = nint((zmax - zmin)/ dz_cfd)
+		
+		allocate(vel_cfd(3,nib,njb,nkb,2))
+
+		! vel_fromCFD cell index from which continum velocity is collected
+		!jb_constrain =	  njb - 1 ! the second row of cells from the top
+		
+	endif
+	
+	if (iter .eq. 1) then
+		! get the previous value of CFD velocities
+		! this call must be global at the momment
+		! because the whole CFD grid is transferred
+		! it will be optimised later
+		call coupler_md_get_CFDvel(overlap, vel_cfd)
+	endif
+
+	if ( .not. overlap) return
+
+
+	! at first CFD step we don't have two values to extrapolate CFD velocities, set inv_dtCFD=0
+	if (ncalls .eq. 0) then
+		inv_dtCFD = 0.0
+	else
+		inv_dtCFD = 1.0/coupler_md_get_dt_cfd()
+	endif
+	ncalls = ncalls + 1
+
+! allocate the needed arrays
+ 
+	allocate(box_average(nib,njb,nkb))	  
+
+	do kb = 1, ubound(box_average,dim=3)
+		do jb = 1, ubound(box_average,dim=2)
+			do ib = 1, ubound(box_average,dim=1)
+				box_average(ib,jb,kb)%np   = 0
+				box_average(ib,jb,kb)%v(:) = 0.0d0
+				box_average(ib,jb,kb)%a(:) = 0.0d0
+			enddo
+		enddo
+	enddo
+
+	! get the range of j cell index in y direction
+
+	js = min(ncells(2),ceiling(ymin/cellsidelength(2))) + nh
+	je = min(ncells(2),ceiling(ymax/cellsidelength(2))) + nh
+
+!	find the maximum number of molecules and allocate a list array	   
+	np_overlap = 0 ! number of particles in overlapping reg
+
+	do k = nh + 1, nh + 1 + ncells(3)
+		do j = js, je
+			do i = nh + 1, nh + 1 + ncells(1) 
+				np_overlap =np_overlap + cell%cellnp(i,j,k)
+			enddo
+		enddo
+	enddo
+
+	allocate(list(4,np_overlap))
+
+	np_overlap = 0
+
+    do k = nh + 1, nh + 1 + ncells(3)
+		do j = js, je
+			do i = nh + 1, nh + 1 + ncells(1) 
+
+				if ( cell%cellnp(i,j,k) == 0) cycle
+			 
+                current => cell%head(i,j,k)%point
+				
+				do ip = 1, cell%cellnp(i,j,k) 
+
+					m = current%molno
+					
+					! get the CFD cell coordinates	
+                    ! apply the force only for the particle which are in domain
+                    ! exchange of particles outside of domain to be added later 
+					if ( r(m,2) >  ymin            .and. r(m,2) < ymax          .and. &
+                         r(m,1) >= -halfdomain(1)  .and. r(m,1) < halfdomain(1) .and. &
+                         r(m,3) >= -halfdomain(3)  .and. r(m,3) < halfdomain(3) ) then
+                        ib = ceiling( (r(m,1) -	 xmin) / dx_cfd)
+                        jb = 1
+                        kb = ceiling( (r(m,3) - zmin ) / dz_cfd)
+
+                        np_overlap = np_overlap + 1
+                        list(1:4, np_overlap) = (/ m, ib, jb, kb /)
+                        
+                        box_average(ib,jb,kb)%np   =  box_average(ib,jb,kb)%np + 1
+                        box_average(ib,jb,kb)%v(:) =  box_average(ib,jb,kb)%v(:) + v(ip,:)
+                        box_average(ib,jb,kb)%a(:) =  box_average(ib,jb,kb)%a(:) + a(ip,:)
+                    endif
+				 
+					current => current%next
+				enddo
+			enddo
+		enddo
+	enddo
+	
+	! here we should have the cell coordinates for the particle ip which is 
+	! in the overlap region
+	! one has to treat separatley the particle that have left the domain
+	! compute the average force for each bin
+
+	!write(0,*)'MD before average over bin. np_overlap', np_overlap
+
+	call average_over_bin
+
+	!write(0,*) 'MD: end simulation_apply_continuum_forces', myid
+
+contains
+
+!=============================================================================
+! Get velocity from CFD and apply force to molecule
+!-----------------------------------------------------------------------------
+
+	subroutine average_over_bin
+		implicit none
+
+		integer ib, jb, kb, i, ip, n
+		real(kind=kind(0.d0)) alpha(3), u_cfd_t_plus_dt(3), inv_dtMD, acfd
+
+
+		! set the continnum constraints for the particle in the bin
+		! speed extrapolation 
+		! add all up
+		inv_dtMD =1.d0/delta_t
+
+		!write(0,'(a,I7,2E12.4)') "MD continuum np, vel_fromCFD1 : ", np_overlap, &
+		!						  maxval(a(list(1,1:np_overlap),:)), &
+		!						  minval(a(list(1,1:np_overlap),:))
+
+		do i = 1, np_overlap
+			ip = list(1,i)
+			ib = list(2,i)
+			jb = list(3,i)
+			kb = list(4,i)
+
+			n = box_average(ib,jb,kb)%np
+
+			!write(0,'(a,4I4,14E12.4)') "MD continuum force", ib,jb,kb,n,box_average(ib,jb,kb)%v(:), &
+			!	box_average(ib,jb,kb)%a(:),v(ip,:),a(ip,:),inv_dtMD,inv_dtCFD
+
+			if ( n .eq. 0 ) cycle
+
+			! using the following exptrapolation formula for continuum velocity
+			! y = (y2-y1)/(x2-x1) * (x-x2) +y2
+			alpha(1) = inv_dtCFD*(vel_cfd(1,ib,1,kb,1) - &
+				vel_cfd(1,ib,1,kb,2))
+
+			u_cfd_t_plus_dt(1) = alpha(1) * (iter + 1)*delta_t + vel_cfd(1,ib,1,kb,1) 
+
+			acfd =	- box_average(ib,jb,kb)%a(1) / n - inv_dtMD * & 
+				( box_average(ib,jb,kb)%v(1) / n - u_cfd_t_plus_dt(1) )
+			a(ip,1) = a(ip,1) + acfd
+
+			!	write(0,'(a,4I4,15E12.4)') "MD continuum force 2", ib,jb,kb,n, &
+			!	 alpha(1),u_cfd_t_plus_dt(1),vel_fromCFD(1,ib,jb+jb_offset,kb,itm1),&
+			!	 vel_fromCFD(1,ib,jb+jb_offset,kb,itm2),&
+			!	 a(ip,1),acfd, r(ip,2) 
+
+		enddo
+
+
+		!	write(400+10*ncalls+myid,'(a,I7,2E12.4)') "MD continuum np, vel_fromCFD 2: ", np_overlap, &
+		!						   maxval(a(list(1,1:np_overlap),:)), &
+		!						   minval(a(list(1,1:np_overlap),:))
+		!	write(400+10*ncalls+myid,'(a,2E12.4)')" inv_dtCFD, inv_dtMD ", inv_dtCFD, inv_dtMD
+		!	do kb=1,nkb
+		!	do jb=1,njb
+		!	do ib=1,nib
+		!		write(400+10*ncalls+myid,'(12E12.4,I7)') vel_fromCFD(:,ib,jb,kb,1), vel_fromCFD(:,ib,jb,kb,2),&
+		!						  box_average(ib,jb,kb)%v(:), box_average(ib,jb,kb)%a(:),&
+		!						  box_average(ib,jb,kb)%np
+		!	enddo
+		!	enddo
+		!	enddo
+
+	end subroutine average_over_bin
+
+end subroutine apply_continuum_forces
+
 
 #endif
 
