1527d5faa936e14a02f2db8dd254b1d481bfa87b
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-04-16 18:46:08 +0000

    Extended setup/restart debug script to show the differences more clearly and to plot a graph of total energy differences vs. iteration.
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@192 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/debug_scripts/test_serial_parallel.sh
+++ src_code/debug_scripts/test_serial_parallel.sh
@@ -25,8 +25,10 @@ echo "Restart file generated OK"
 mv ./results/final_state ./debug_scripts/
 mv ./debug_scripts/final_state ./debug_scripts/debug_initial
 ./md.exe -i ./debug_scripts/input_2048periodic.in -r ./debug_scripts/debug_initial > ./debug_scripts/serial.out
+mv ./results/macroscopic_properties ./debug_scripts/serial.macro
 echo "Serial run OK"
 cat ./debug_scripts/serial.out  >> ./debug_scripts/debug_log
+
 
 #Run parallel code to generate output
 make clean >> ./debug_scripts/debug_log
@@ -34,14 +36,20 @@ make debug_p >> ./debug_scripts/debug_log
 echo "Parallel Build OK"
 mpiexec -n 8 ./md.exe -i ./debug_scripts/input_2048periodic.in -r ./debug_scripts/debug_initial > ./debug_scripts/parallel.out 
 cat ./debug_scripts/parallel.out >> ./debug_scripts/debug_log
+mv ./results/macroscopic_properties ./debug_scripts/parallel.macro
 echo "Parallel run OK"
 make clean
 
 #Check serial and parallel outputs are the same
 #awk -F "\"*;\"*" '{print $2}' ./debug_scripts/serial.out   > ./debug_scripts/s_$2
 #awk -F "\"*;\"*" '{print $2}' ./debug_scripts/parallel.out > ./debug_scripts/p_$2
-diff -B --suppress-common-lines ./debug_scripts/serial.out ./debug_scripts/parallel.out >./debug_scripts/diff_serialparallel.out
+#diff -B --suppress-common-lines ./debug_scripts/serial.out ./debug_scripts/parallel.out >./debug_scripts/diff_serialparallel.out
 
-echo "Difference between serial and parallel"
-cat ./debug_scripts/diff_serialparallel.out
+./debug_scripts/test_rundiff.py
+echo "Difference between serial and parallel macroscopic properties"
+cat ./debug_scripts/diff.macro
 
+echo "set title \"Serial - Parallel Total Energies\"
+set xlabel \"iter\"
+set ylabel \"Energy (LJU)\"
+plot \"./debug_scripts/diff.macro\" u 1:7 w p lc 1 t \"TE\"" | gnuplot -persist
