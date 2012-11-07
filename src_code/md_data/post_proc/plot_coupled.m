f080dba50b3dbdb850c0c7be2dab368760454014
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-11-07 11:17:18 +0000

    Improved plot_coupled.m script to read MD step by step along with other asphetic improvements
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/coupler_dCSE@360 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/md_data/post_proc/plot_coupled.m
+++ src_code/md_data/post_proc/plot_coupled.m
@@ -7,7 +7,7 @@ CFD = 1;
 
 %Find results files
 %resultfile_dir = './../results/';
-resultfile_dir = '/home/es205/results/CX1_data/CPL_testing/48000_iter/';
+resultfile_dir = '/home/es205/results/CX1_data/CPL_testing/multiple_cells/';
 %resultfile_dir = '/home/es205/results/MD_continuum_results/code/coupled_couette/varying_processor_study/coupler_dCSE/src_code/';
 resultfile_dir_md = strcat(resultfile_dir,'md_data/results/');
 resultfile_dir_cfd = strcat(resultfile_dir,'couette_data/'); 
@@ -20,24 +20,12 @@ if (CFD == 0)
     read_continuum_header
 elseif(CFD == 1)
     %---Get CFD grid size ----
-    %read_grid(strcat(resultfile_dir_cfd,'grid.data'),[1 1 1])
     [ngx, ngy, ngz, Lx, Ly, Lz, dx, dy, dz] = read_report(strcat(resultfile_dir_cfd,'report'));
-    %ngz = 8+1;
-
-    %--- CFD domain size ----
-    %Lx = max(x_grid);
-    %Ly = max(y_grid);
-    %Lz = 12.7;
-
-
 end
 
+% = = = Read MD header = = = 
 %Check output flags and read data accordingly
 if (velocity_outflag == 4)
-    filename = strcat(resultfile_dir_md,'/mbins');
-    mass_bins=read_mbins(filename,resultfile_dir_md);
-    filename = strcat(resultfile_dir_md,'/vbins');
-    vel_bins=read_vbins(filename,resultfile_dir_md);
     Nvel_records = floor((Nsteps-initialstep) / (tplot * Nvel_ave));
 elseif ((velocity_outflag > 0) & (velocity_outflag < 4) )
     read_vslice
@@ -47,6 +35,7 @@ elseif ((mass_outflag > 0) & (mass_outflag < 4) )
     read_mslice
 end
 
+% = = = Read CFD data = = =
 if (CFD == 0) 
 	if (continuum_vflag == 3)
 	    read_continuum_vbins
@@ -60,7 +49,7 @@ end
 
 
 
-%continuum_Domain_setup
+%Continuum_Domain_setup
 %set(0,'currentfigure',fig1)
 %read_grid(strcat(resultfile_dir_cfd,'grid.data'),[1 1 1],'plot')
 %%MD domain set-up
@@ -72,61 +61,25 @@ Domain_setup
 %Plot Virial Stress
 %read_pvirial
 
-%Calculate Reynolds number
-%visc = 1.6;
-%L = 1; %liquiddomain(velocity_outflag)/globaldomain(velocity_outflag);
-%Re = 0.8*L*max(wallslidev)/visc;
+% %Average multiple MD cells into one
+% MD_cells_per_CFD = 1;
+% 
+% switch MD_cells_per_CFD
+% case 1
+%     ave_vel_slice = vel_slice;
+%     ave_mass_slice = mass_slice;
+% case 2
+%     ave_vel_slice = zeros(size(vel_slice,1)/MD_cells_per_CFD,size(vel_slice,2),size(vel_slice,3));
+%     ave_mass_slice = zeros(size(mass_slice,1)/MD_cells_per_CFD,size(mass_slice,2),size(mass_slice,3));
+%     n=1;
+%     for i=1:MD_cells_per_CFD:size(vel_slice,1)
+%         ave_vel_slice(n,:,:) = 0.5*(vel_slice(i,:,:) + vel_slice(i+1,:,:));
+%         ave_mass_slice(n,:,:) = 0.5*(mass_slice(i,:,:)+mass_slice(i,:,:));
+%         n = n + 1;
+%     end
+% end
 
-%Calculate spanwise direction
-if (velocity_outflag < 4)
-    ixyz = velocity_outflag;
-else
-    %Find location of smallest dimension and take slice through this
-    ixyz = 2 %find(nbinsliquid==min(nbinsliquid));
-    jxyz = mod(ixyz+1,3)+1;
-    kxyz = mod(ixyz,3)+1;
-    mass_slice = squeeze(sum(sum(mass_bins(:,:,:,:),jxyz),kxyz)/(nbins(jxyz)*nbins(kxyz)));
-    vel_slice =  squeeze(sum(sum(vel_bins(:,:,:,:,:),jxyz),kxyz)/(nbins(jxyz)*nbins(kxyz)));
-end
-
-clear mass_bins vel_bins
-
-%Average same as slice
-% t_ave = 200;
-% %ave_vel_slice(:,:,1) = mean(vel_slice(:,:,    0 + 1:  t_ave),3);
-% ave_vel_slice(:,:,1) = mean(vel_slice(:,:,  t_ave+1:2*t_ave),3);
-% ave_vel_slice(:,:,2) = mean(vel_slice(:,:,2*t_ave+1:3*t_ave),3);
-% ave_vel_slice(:,:,3) = mean(vel_slice(:,:,3*t_ave+1:4*t_ave),3);
-% ave_vel_slice(:,:,4) = mean(vel_slice(:,:,4*t_ave+1:5*t_ave),3);
-% %ave_mass_slice(:,1) = mean(mass_slice(:,    0 + 1:  t_ave),2);
-% ave_mass_slice(:,1) = mean(mass_slice(:,  t_ave+1:2*t_ave),2);
-% ave_mass_slice(:,2) = mean(mass_slice(:,2*t_ave+1:3*t_ave),2);
-% ave_mass_slice(:,3) = mean(mass_slice(:,3*t_ave+1:4*t_ave),2);
-% ave_mass_slice(:,4) = mean(mass_slice(:,4*t_ave+1:5*t_ave),2);
-
-%Average multiple MD cells into one
-MD_cells_per_CFD = 1;
-
-switch MD_cells_per_CFD
-case 1
-    ave_vel_slice = vel_slice;
-    ave_mass_slice = mass_slice;
-case 2
-    ave_vel_slice = zeros(size(vel_slice,1)/MD_cells_per_CFD,size(vel_slice,2),size(vel_slice,3));
-    ave_mass_slice = zeros(size(mass_slice,1)/MD_cells_per_CFD,size(mass_slice,2),size(mass_slice,3));
-    n=1;
-    for i=1:MD_cells_per_CFD:size(vel_slice,1)
-        ave_vel_slice(n,:,:) = 0.5*(vel_slice(i,:,:) + vel_slice(i+1,:,:));
-        ave_mass_slice(n,:,:) = 0.5*(mass_slice(i,:,:)+mass_slice(i,:,:));
-        n = n + 1;
-    end
-end
-
-%Average velocity per molecule
-for n =1:3
-    vel_slice(:,n,:) = squeeze(vel_slice(:,n,:))./mass_slice;
-    ave_vel_slice(:,n,:) = squeeze(ave_vel_slice(:,n,:))./ave_mass_slice;
-end
+MD_cells_per_CFD = 1
 
 %Plot mass and velocity
 if (CFD == 0) 
@@ -154,35 +107,66 @@ elseif(CFD == 1)
     timeratio = 4; %delta_t/1.2;
 end
 
-
-u_0 = 1; t_0 = 160;
+%Get orthogonal direction
+if (velocity_outflag < 4)
+    ixyz = velocity_outflag;
+else
+    ixyz = 2;
+    jxyz = mod(ixyz+1,3)+1;
+    kxyz = mod(ixyz,3)+1;
+end
 
 %Analytical Solution
+u_0 = 1; t_0 = 160;
 spectral_res = 6;
 viscosity = 1.6;
-Re = density*u_0*coupleddomain(ixyz)/viscosity;
-analy_points = 20;%spectral_res*(nbins(ixyz)); % Number of spectral points
+Re = density*u_0*1/viscosity;
+analy_points = 20; % Number of spectral points
 xaxis_analy = 0:1/20:1.00; %As the liquid solid interaction is still 1, the domain is moved in slightly due to molecular sticking at the wall
-
-%Write a sequence of frames to a compressed AVI file, couette.avi:
-% Prepare the new file.
-%vidObj = VideoWriter('couette.avi');
-%vidObj.FrameRate = 10;
-%open(vidObj);
-
 
 %setup figures
 scrsz = get(0,'ScreenSize');
 %fig1 = figure('Position',[1 scrsz(4)/4 scrsz(3)/6 scrsz(4)/2]);
-fig2 = figure('Position',[scrsz(3)/6 scrsz(4)/4 scrsz(3)/4 scrsz(4)/2]);
+fig2 = figure('Position',[scrsz(3)/6 scrsz(4)/4 scrsz(3)/5 scrsz(4)/2]);
 gax = axes('Position', [0.1, 0.1, 0.8, 0.8]);
-hax = axes('Position', [.13, .48, .23, .4]);
+hax = axes('Position', [.18, .48, .23, .35]);
 
+%Write a sequence of frames to a compressed AVI file, couette.avi:
+% Prepare the new file.
+% vidObj = VideoWriter('couette.avi');
+% vidObj.FrameRate = 10;
+% open(vidObj);
+
+intial_offset = 53.33;
 t_ave = 1;  %Average over timesteps
 m = 0+t_ave; %Initial Timestep
 for i = 1:Nvel_records
-
     i
+
+    %Read velocity
+    filename = strcat(resultfile_dir_md,'/mbins');
+    mass_bins = read_mbins(filename,resultfile_dir_md,m);
+    filename = strcat(resultfile_dir_md,'/vbins');
+    vel_bins = read_vbins(filename,resultfile_dir_md,m);
+
+    %Calculate spanwise direction
+    if (velocity_outflag < 4)
+        ixyz = velocity_outflag;
+    else
+        %Find location of smallest dimension and take slice through this
+        %ixyz = find(nbinsliquid==min(nbinsliquid));
+        ixyz = 2;
+        jxyz = mod(ixyz+1,3)+1;
+        kxyz = mod(ixyz,3)+1;
+        mass_slice = squeeze(sum(sum(mass_bins(:,:,:),jxyz),kxyz)/(nbins(jxyz)*nbins(kxyz)));
+        vel_slice =  squeeze(sum(sum(vel_bins(:,:,:,:),jxyz),kxyz)/(nbins(jxyz)*nbins(kxyz)));
+    end
+
+    %Average velocity per molecule
+    for n =1:3
+        ave_vel_slice(:,n) = vel_slice(:,n)./mass_slice';
+    end
+
     %Plot density fluctuations in domain
 %     set(0,'currentfigure',fig1)
 %     plot(xaxis(1:(gnbins(ixyz))),mean(density_slice(:,:),2),'--');
@@ -191,11 +175,13 @@ for i = 1:Nvel_records
 %     hold off
 
     %Time -0.5 to record value at half time interval
-    %if (m > 200) 
-        t =(400+m-0.5)*500%delta_t*Nmass_ave*tplot;
-    %else
-    %    t = 0;
-    %end
+    t =(intial_offset+m-0.5)*2*delta_t*Nmass_ave*tplot;
+    
+    % =================================
+    %    Plot MD region on full axis
+	% =================================
+    set(0,'currentfigure',fig2)
+    set(fig2,'CurrentAxes',gax)
 
     
     % =================================
@@ -208,10 +194,17 @@ for i = 1:Nvel_records
     plot(linspace(MD_domain(ixyz)-overlap(ixyz)+0.5*cfd_binsize(ixyz),coupleddomain(ixyz)-0.5*cfd_binsize(ixyz),ngy-1)/coupleddomain(ixyz),continuum_velslice(:,m)/u_0,'s','Color',[.5 .5 .5],'MarkerSize',10,'LineWidth',5);
     hold on
     %plot molecular velocity profile
-    plot(linspace(-0.5*cfd_binsize(ixyz),MD_domain(ixyz)-0.5*cfd_binsize(ixyz),gnbins(ixyz)/MD_cells_per_CFD)/coupleddomain(ixyz),mean(ave_vel_slice(:,1,m),3),'x','LineWidth',5,'Color',[.2 .2 .2],'MarkerSize',10);
+    plot(linspace(-0.5*cfd_binsize(ixyz),MD_domain(ixyz)-0.5*cfd_binsize(ixyz),gnbins(ixyz)/MD_cells_per_CFD)/coupleddomain(ixyz),ave_vel_slice(:,1),'x','LineWidth',5,'Color',[.2 .2 .2],'MarkerSize',10);
       
+    %Plot anayltical solution
+    analy = couette_analytical_fn(t,Re,[1,0],coupleddomain(ixyz), ...
+                                   analy_points,'top');
+    plot(xaxis_analy,analy/u_0,'k','LineWidth',4);
+
     %Make plot look nice
     %legend ('MD','CFD','CFD_{halo}','location','NorthEast'); legend('boxoff')
+    set(gca,'FontSize',20)
+    axis([-0.1 1.1 -0.1 1.1]);
     set(gca,'FontSize',20)
     axis([-0.1 0.65 -0.05 0.25]);
     xlabel('y/H'); ylabel('U_x/U')
@@ -220,63 +213,55 @@ for i = 1:Nvel_records
     hold off
 
     % =================================
-    %    Plot Insert of full axis
+    %    Plot Insert 
 	% =================================
     set(fig2,'CurrentAxes',hax)
 
-    plot(linspace(MD_domain(ixyz)-overlap(ixyz)+0.5*cfd_binsize(ixyz),coupleddomain(ixyz)-0.5*cfd_binsize(ixyz),ngy-1)/coupleddomain(ixyz),continuum_velslice(:,m)/u_0,'s','Color',[.5 .5 .5],'MarkerSize',10,'LineWidth',5);
+    plot(linspace(MD_domain(ixyz)-overlap(ixyz)+0.5*cfd_binsize(ixyz),coupleddomain(ixyz)-0.5*cfd_binsize(ixyz),ngy-1)/coupleddomain(ixyz),continuum_velslice(:,m)/u_0,'s','Color',[.5 .5 .5],'MarkerSize',5,'LineWidth',2);
     hold on
-	plot(linspace(-0.5*cfd_binsize(ixyz),MD_domain(ixyz)-0.5*cfd_binsize(ixyz),gnbins(ixyz)/MD_cells_per_CFD)/coupleddomain(ixyz),mean(ave_vel_slice(:,1,m),3),'x','LineWidth',5,'Color',[.2 .2 .2],'MarkerSize',10);
+	plot(linspace(-0.5*cfd_binsize(ixyz),MD_domain(ixyz)-0.5*cfd_binsize(ixyz),gnbins(ixyz)/MD_cells_per_CFD)/coupleddomain(ixyz),ave_vel_slice(:,1),'x','LineWidth',5,'Color',[.2 .2 .2],'MarkerSize',5);
+
+
+    %Fix axis position to averaged continuum BC
+    tave_vbc = 10;
+    if (m < tave_vbc+1)
+        ave_vbc = mean(continuum_velslice(1,m:m+tave_vbc));
+    elseif(m > Nvel_records-tave_vbc-1)
+        ave_vbc = mean(continuum_velslice(1,m-tave_vbc:m));
+    else
+        ave_vbc = mean(continuum_velslice(1,m-tave_vbc:m+tave_vbc));
+    end
+	axis([0.35 0.6 ave_vbc-0.025 ave_vbc+0.1 ])
     analy = couette_analytical_fn(t,Re,[1,0], ...
         coupleddomain(ixyz), ...
         analy_points,'top');
     %analy = startup_plate_analytical_fn(t,t_0,viscosity,density,u_0,liquiddomain(ixyz),analy_points);
     plot(xaxis_analy,analy/u_0,'k','LineWidth',4);
  	set(hax,'XTick',[]); 
-	axis([-0.1 1.1 -0.1 1.1])
-
-%	axis([ (MD_domain(ixyz)-overlap(ixyz)+0.5*cfd_binsize(ixyz))/coupleddomain(ixyz)-0.2 MD_domain(ixyz)/coupleddomain(ixyz)+0.2 -0.01 0.01])
     hold off
+
     drawnow
     pause(0.1)
+
     %Store pictures and videos
     %if (mod(m,Nvel_records/10) == 0)
-    %    savefig(strcat('Velocity_',num2str(i)),'png')
+    %    savefig(strcat('Velocity_',num2str(m)),'png')
     %end
-   %currFrame = getframe(gcf);
-   %writeVideo(vidObj,currFrame);
-    
+% 	currFrame = getframe(gcf);
+% 	writeVideo(vidObj,currFrame);
+%     
     %Increase plot counter and check for end of file
     if (m > Nvel_records)
         break
     end
     m = m+1
     if (m > Nvel_records)
-        m = Nvel_records-3
+        m = Nvel_records-30
     end
 end
 
 % Close the file.
-%close(vidObj)
+% close(vidObj)
 
 %movefile ./couette.avi ./../results
-% 
-% for m = 1:Ncontinuum_records
-%      z = [continuum_velslice(2:ny+1,m)/u_wall,continuum_velslice(2:ny+1,m)/u_wall, continuum_velslice(2:ny+1,m)/u_wall, ...
-%           continuum_velslice(2:ny+1,m)/u_wall,continuum_velslice(2:ny+1,m)/u_wall,continuum_velslice(2:ny+1,m)/u_wall, ...
-%           continuum_velslice(2:ny+1,m)/u_wall];
-%     imagesc(z);
-%     colormap(gray)
-%   
-% 
-%     [x,y] = meshgrid(xaxis(((gnbins(ixyz)-1)/2)+1-overlap:end-1),0:1/6:1);
-%     z = [continuum_velslice(2:ny+1,m)/u_wall,continuum_velslice(2:ny+1,m)/u_wall, continuum_velslice(2:ny+1,m)/u_wall, ...
-%          continuum_velslice(2:ny+1,m)/u_wall,continuum_velslice(2:ny+1,m)/u_wall,continuum_velslice(2:ny+1,m)/u_wall, ...
-%          continuum_velslice(2:ny+1,m)/u_wall];
-%     mesh(x,y,z')
-%     gray
-%     axis([0 1 0 1 0 1])
-%      pause(0.01)  
-%  end
-
 
