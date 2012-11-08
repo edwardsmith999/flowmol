133db9703e35a8701b20bd6899751377e3e6d77e
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-11-08 11:13:37 +0000

    Added routines to package/unpackage all inputs for a coupled run. This will improve sharing of cases between users/computers. Instructions for use : Assuming Bob wants to share his current coupled case with Alice, he runs the ./inputs/pack_case.sh script with a sensible name for the case (used to create a folder & subversion comments). This will create and commit the case to the server. Alice then uses svn update in her version of the ./input folder and calls ./inputs/unpack_case.sh to overwrite all her current coupler input files. NOTE: The DNS code may require ./CFD_dCSE/src_code/change_grid.sh to be run and the entire coupled code should be rebuilt before use
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@362 1e92d776-e0e5-45cd-b670-8958d04d46c8

---
+++ inputs/pack_case.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Packs input folders and adds to subversion server to share with other users
+#
+if [ $# -lt 1 ]; then
+	echo "Input of the form pack_case.sh [Case Name]"
+else
+	mkdir $1
+	cp ./../CFD_dCSE/src_code/grid_generation/input.file ./$1
+	cp ./../CFD_dCSE/src_code/setup/input.setup ./$1
+	cp ./../CFD_dCSE/src_code/main_code/param.inc ./$1
+	cp ./../CFD_dCSE/src_code/main_code/input ./$1
+	cp ./../coupler_dCSE/src_code/COUPLER.in ./$1
+	cp ./../coupler_dCSE/src_code/md_data/MD.in ./$1
+	svn add $1
+	echo "Case" $1 "added to input folder - use upack_case.sh [" $1 "] in ./input folder to use" > ./$1/log 
+	svn ci $1 -F ./$1/log
+fi
