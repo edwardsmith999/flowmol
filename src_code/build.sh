b9961206460345772c9f02403d37dc70df1c4dcd
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-11-27 16:38:29 +0000

    Removed the following files from subversion repo ./md_data/MD.in ./build.sh ./topology_test/input ./COUPLER.in ./../../CFD_dCSE/src_code/grid_generation/input.file ./../../CFD_dCSE/src_code/setup/input.setup. Run unpack to replace them on new checkout
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/coupler_dCSE@390 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-BUILDtype=opt
-if [ $# -lt 1 ]; then
-	cd ./../../;  make BUILD=$BUILDtype couette_md  ;cd ./coupler_dCSE/src_code/;
-else
-	cd ./../../; make clean_all;  make BUILD=$BUILDtype couette_md;cd ./coupler_dCSE/src_code/;
-fi
