63b9de1e89a05bc9c5805d6239fe7b87d1daf851
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2012-11-30 18:34:23 +0000

    Added coupler input parameter tuning python script.
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@393 1e92d776-e0e5-45cd-b670-8958d04d46c8

---
+++ coupler_dCSE/src_code/CPL_parameter_tuning.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import math
+
+# "Inputs"
+density = 0.8
+rcutoff = 2.2
+dr_nbr  = 0.29
+d_xL    = 52.1
+d_zL    = 52.1 
+d_yL_md = 31.3
+d_yL_cfd = 52.1 - 15.6
+cellsize_ratio = 2.0 # must be a float representation of an integer
+
+# Calculate number of initial units required to get close to desired yL_md
+a = 1.0 / (math.pow((density/4.0),(1.0/3.0)))    # lattice parameter a
+nunits_x_md = int(round(d_xL/a))                 # best number of units
+nunits_y_md = int(round(d_yL_md/a))              # best number of units
+nunits_z_md = int(round(d_zL/a))                 # best number of units
+xL_md = nunits_x_md*a                            # new xL_md
+yL_md = nunits_y_md*a                            # new yL_md
+zL_md = nunits_z_md*a                            # new zL_md
+
+# Calculate computational cell size from rcutoff and yL_md (floor to be
+# conservative)
+ncells_x_md = int(math.floor(xL_md / (rcutoff + dr_nbr)))
+ncells_y_md = int(math.floor(yL_md / (rcutoff + dr_nbr)))
+ncells_z_md = int(math.floor(zL_md / (rcutoff + dr_nbr)))
+dx_md = xL_md / float(ncells_x_md)
+dy_md = yL_md / float(ncells_y_md)
+dz_md = zL_md / float(ncells_z_md)
+
+# Calculate cfd cell size and closest possible yL_cfd  
+
+dx_cfd = dx_md*cellsize_ratio
+dy_cfd = dy_md*cellsize_ratio
+dz_cfd = dz_md*cellsize_ratio
+
+xL_cfd = xL_md  # Constraint
+zL_cfd = zL_md  # Constraint
+
+ncells_x_cfd = int(round(xL_cfd/dx_cfd))
+ncells_y_cfd = int(round(d_yL_cfd/dy_cfd))
+ncells_z_cfd = int(round(zL_cfd/dz_cfd))
+
+yL_cfd = ncells_y_cfd*dy_cfd
+
+message = (
+           "Inputs: \n" + 
+           "\tdensity:           \t" + str(density)        + "\n" + 
+           "\trcutoff:           \t" + str(rcutoff)        + "\n" + 
+           "\tdr_nbr:            \t" + str(dr_nbr)         + "\n" + 
+           "\tDesired xL:        \t" + str(d_xL)           + "\n" + 
+           "\tDesired zL:        \t" + str(d_zL)           + "\n" + 
+           "\tDesired MD yL:     \t" + str(d_yL_md)        + "\n" + 
+           "\tDesired CFD yL:    \t" + str(d_yL_cfd)       + "\n" + 
+           "\tCellsize ratio:    \t" + str(cellsize_ratio) + "\n" + 
+
+           "Results: \n" + 
+           "\tMD FCC units(x):   \t" + str(nunits_x_md)    + "\n" + 
+           "\tMD FCC units(y):   \t" + str(nunits_y_md)    + "\n" + 
+           "\tMD FCC units(z):   \t" + str(nunits_z_md)    + "\n" + 
+           "\tMD ncells(x):      \t" + str(ncells_x_md)    + "\n" +  
+           "\tMD ncells(y):      \t" + str(ncells_y_md)    + "\n" +  
+           "\tMD ncells(z):      \t" + str(ncells_z_md)    + "\n\n" +  
+  
+           "\tMD  xL:            \t" + str(xL_md)          + "\n" + 
+           "\tCFD xL:            \t" + str(xL_cfd)         + "\n" + 
+           "\tMD  yL:            \t" + str(yL_md)          + "\n" + 
+           "\tCFD yL:            \t" + str(yL_cfd)         + "\n" + 
+           "\tMD  zL:            \t" + str(zL_md)          + "\n" + 
+           "\tCFD zL:            \t" + str(zL_cfd)         + "\n\n" + 
+  
+           "\tCFD ncells(x):     \t" + str(ncells_x_cfd)   + "\n" +  
+           "\tCFD ncells(y):     \t" + str(ncells_y_cfd)   + "\n" +  
+           "\tCFD ncells(z):     \t" + str(ncells_z_cfd)   + "\n" +  
+		   "\tMD cell length(x): \t" + str(dx_md)          + "\n" +  
+           "\tMD cell length(y): \t" + str(dy_md)          + "\n" +  
+           "\tMD cell length(z): \t" + str(dz_md)          + "\n" +  
+           "\tCFD cell length(x):\t" + str(dx_cfd)         + "\n" +  
+           "\tCFD cell length(y):\t" + str(dy_cfd)         + "\n" +  
+           "\tCFD cell length(z):\t" + str(dz_cfd)
+          )
+print(message)
