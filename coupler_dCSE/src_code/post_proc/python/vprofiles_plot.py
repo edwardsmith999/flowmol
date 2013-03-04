92aa42cb59a8a1aaff2e2a40979e0a1c5af55ca7
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-03-04 10:49:22 +0000

    A few cosmetic updates to the python visualisation scripts...
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@452 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- coupler_dCSE/src_code/post_proc/python/vprofiles_plot.py
+++ coupler_dCSE/src_code/post_proc/python/vprofiles_plot.py
@@ -3,8 +3,9 @@ from PlotCoupled import *
 
 #wdir = '/home/djt06/Documents/Academia/PhD/Code/Data/2013/02/19/NCER/branch/coupler_dCSE/src_code/'
 
+wdir = '/home/djt06/Desktop/LJ2/branch/coupler_dCSE/src_code/'
 CPLfig = PlotCoupled(wdir)
-for rec in [1,3,7,15,31]:
+for rec in [7,15,31,63,127]:
 	CPLfig.plot_singlerec(rec)
 
 plt.show()
