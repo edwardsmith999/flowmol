eb1ad4d7bb24405ebf9dc802a2fcf4eb6f4eb611
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-03-08 00:22:26 +0000

    Corrected bug in Flekkoy case - various corrections made on CX1 were missing from MD directory but didn't show up on subversion
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/coupler_dCSE@454 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/post_proc/python/vprofiles_animate.py
+++ src_code/post_proc/python/vprofiles_animate.py
@@ -2,8 +2,8 @@
 from PlotCoupled import *
 
 #wdir = '/home/djt06/Documents/Academia/PhD/Code/Data/2013/02/19/NCER/branch/coupler_dCSE/src_code/'
-wdir = '/home/djt06/Desktop/LJ2/branch/coupler_dCSE/src_code/'
-
+#wdir = '/home/djt06/Desktop/LJ2/branch/coupler_dCSE/src_code/'
+wdir = '/home/es205/link_to_work/coupled2/coupler_dCSE/src_code/'
 CPLfig = PlotCoupled(wdir)
 CPLfig.start_animation()
 #CPLfig.anim.save('movie',fps=20)
