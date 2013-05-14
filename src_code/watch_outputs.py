9567f2fb4a0114f3fd0bdd3220d225da4725e3b1
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-05-14 15:02:38 +0000

    New syntax for watch_outputs.py: run watch_outputs.py help to get more info
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@501 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/watch_outputs.py
+++ src_code/watch_outputs.py
@@ -1,107 +1,142 @@
 #! /usr/bin/env python
 import sys
 import numpy as np
+import itertools
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-class WatchOutput:
+class DataLine:
 
-	def __init__(self,filename,figureinfo):	
+	colors = itertools.cycle(['b','g','r','c','m','y','k'])
 
-		self.filename = filename
-		self.haxis = figureinfo[0]
-		self.vaxis = figureinfo[1]
+	def __init__(self, linedef):
+
+		self.filename = linedef[0] 
+		self.haxis = linedef[1] 
+		self.vaxis = linedef[2]
+
+		label = self.filename + " " + self.vaxis
+		color = next(DataLine.colors)
+
+		self.line2D = matplotlib.lines.Line2D([],[],color=color,label=label)
 	
-		self.fig = plt.figure()
-		self.ax  = self.fig.add_subplot(111)
+	def update_linedata(self):
 
-		self.lines = []
-		for i in range(len(self.vaxis)):
-			self.lines.append(self.ax.plot([])[0]) # First item of ax.plot
-			self.lines[i].set_data([],[])
+		filedata = np.genfromtxt(self.filename,delimiter=';',names=True)
+		self.line2D.set_xdata(filedata[self.haxis])
+		self.line2D.set_ydata(filedata[self.vaxis])
+	
+class OutputFigure:
 
-		self.setup_anim()
+	def __init__(self,linedefs):
 
-	def setup_anim(self):
+		# Own figure and axes
+		self.fobj = plt.figure()
+		self.ax = self.fobj.add_subplot(111)
+
+		# Create DataLine instances 
+		self.datalines = []
+		for linedef in linedefs:
+			self.datalines.append(DataLine(linedef))
+
+		# Add line2D objects to axis
+		for dataline in self.datalines:
+			self.ax.add_line(dataline.line2D)
+			self.ax.set_xlabel(dataline.haxis)
+
+		# Setup animation
 		self.anim = animation.FuncAnimation(
-		                                    self.fig,
-		                                    self.update,
-		                                    self.data_gen,
+		                                    self.fobj,
+		                                    self.update_axis,
 		                                    interval=50
 	   	                                  )
+		self.ax.legend()
 
-	def update(self,data):
+	def update_axis(self,dummy):
+
+		for dataline in self.datalines:
+			dataline.update_linedata()
+
 		self.ax.relim()
 		self.ax.autoscale_view(True,True,True)
-		for i in range(len(self.vaxis)):
-			haxis = self.haxis
-			vaxis = self.vaxis[i]
-			self.lines[i].set_xdata(data[haxis])
-			self.lines[i].set_ydata(data[vaxis])
-
-	def data_gen(self):
-		while True:
-			filedata = np.genfromtxt(self.filename,delimiter=';',names=True)
-			yield filedata
-
-class Figure:
-
-	def __init__(self,args):
-
-		fname = args[0]
-		haxis = args[1]
-		vaxis = args[2:]
-
-		self.filename = fname 
-		self.figdef = [haxis,vaxis] 
 	
-# -------------------------------------------
 
-def SetupFigures(args):
+class WatchFigures:
 
-	try:
-		figargs = []
-		figures = []
-		for arg in args:
-			if (arg != '+'):
-				figargs.append(arg)
-			else:
-				figures.append(Figure(figargs))
-				figargs = []		
-		figures.append(Figure(figargs))
-		return figures 
-	except:
+	def __init__(self,figuredefs):
+	
+		figures = []	
+		for figuredef in figuredefs:
+			figures.append(OutputFigure(figuredef))
+			
+
+def ParseArguments():
+
+	arguments = sys.argv[1:]
+
+	if (sys.argv[2] == 'help'):
+
 		message = (
-		            "It looks like you did something wrong, so here's \n" +
-		            "some help. Watch_outputs.py requires arguments that \n" +
-		            "specify which figures you would like to watch during \n"+
-		            "the simulation. They take the form: \n\n"+
-		            "  ./watch_outputs.py filename1 horizontal_axis1 \n" +
-		            "                     {vertical_axes1} + filename2 \n "+
-		            "                     horizontal_axis2 {vertical_axes2}\n" +
-		            "                     + ... \n" +
-		            "\n\n " +
-		            "Any number of extra figures may be generated simply by \n"+
-		            "separating arguments with + signs. The arguments \n" +
-		            "horizontal_axis and vertical_axes must be EXACT \n" +
-		            "matches to the first-line header of the columns in \n" +
-		            "the output file you are trying to follow. Let's say, \n"+
-		            "for example, you'd like to follow two figures. One for \n"+
-		            "plotting the energies, and the other for plotting the \n" +
-		            "pressure. The appropriate call would look like: \n\n" +
-		            "  ./watch_outputs.py results/macroscopic_properties \n" +
-		            "                     simtime KE PE TE + \n" +
-		            "                     results/macroscopic_properties \n" +
-		            "                     simtime Pressure \n"
-		          )
+					". Watch_outputs.py requires arguments that \n" +
+					"specify which figures you would like to watch during \n"+
+					"the simulation. They take the form: \n\n"+
+					"  ./watch_outputs.py filename1 horizontal_axis1 \n" +
+					"                     {vertical_axes1} + filename2 \n "+
+					"                     horizontal_axis2 {vertical_axes2}\n" +
+					"                     ++ new figure arguments... \n" +
+					"\n\n " +
+					"Any number of extra figures may be generated simply by \n"+
+					"separating arguments with TWO + signs (++). The arguments \n" +
+					"horizontal_axis and vertical_axes must be EXACT \n" +
+					"matches to the first-line header of the columns in \n" +
+					"the output file you are trying to follow. Let's say, \n"+
+					"for example, you'd like to follow two figures. One for \n"+
+					"plotting the energies, and the other for plotting the \n" +
+					"pressure. The appropriate call would look like: \n\n" +
+					"  ./watch_outputs.py results/macroscopic_properties \n" +
+					"                     simtime KE + \n" +
+					"                     results/macroscipic_properties \n" +
+					"                     simtime PE + \n" +
+					"                     results/macroscopic_properties \n" +
+					"                     simtime TE     ++ \n" +
+					"                     results/macroscopic_properties \n" +
+					"                     simtime Pressure \n"
+				  )
 		print(message)
 		quit()
 
-# --------------------------------------------
+	linedef = []
+	linedefs = []
+	figuredefs = []
+	for arg in arguments:
 
-figures = SetupFigures(sys.argv[1:])
-	
-for f in figures:
-	fig = WatchOutput(f.filename,f.figdef)
+		linedef.append(arg)
 
+		if ( '+' in arg ):
+			linedef.pop()
+
+		if ( arg == '+' ):
+
+			linedefs.append(linedef)	
+			linedef = []
+
+		elif ( arg == '++' ):
+
+			linedefs.append(linedef)
+			figuredefs.append(linedefs)
+			linedef = []
+			linedefs = []
+
+	linedefs.append(linedef)
+	figuredefs.append(linedefs)
+
+	return figuredefs
+
+
+##############################################################################
+# Parse the command line arguments and try plotting!
+
+figure_definitions = ParseArguments()
+Watch = WatchFigures(figure_definitions)
 plt.show()
