ce127b9052a3ef8268f1270a1d74b28411adf549
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-05-24 14:49:45 +0000

    Fixed a missing variable definition bug in external force
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@506 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/external_forces.f90
+++ src_code/external_forces.f90
@@ -32,7 +32,7 @@ subroutine simulation_apply_global_force(ixyz,F_const)
 	use module_external_forces
 	implicit none
 
-	
+	integer				 		 :: n
 	integer,intent(in)     			:: ixyz
 	double precision,intent(in)		:: F_const
 
