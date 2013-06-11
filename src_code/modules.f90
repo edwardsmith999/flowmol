69a9389415d423c6738bd25acc8f2a396b93a419
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-06-11 16:11:00 +0000

    Removed domega_dt from code. This was dangerous when the ramplength (i.e. dt) was zero, and was unnecessary anyway...
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@512 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/modules.f90
+++ src_code/modules.f90
@@ -375,7 +375,6 @@ module concentric_cylinders
 	real(kind(0.d0)) :: omega                !Actual angular vel
 	real(kind(0.d0)) :: omega_i, omega_f     !Initial and final angular vel
 	real(kind(0.d0)) :: omega_ramplength     !Time over which to ramp omega
-	real(kind(0.d0)) :: domega_dt            !Ramp gradient
 	integer :: omega_rampiters               !Iterations over which to ramp omega
 
 	character(len=200) :: cyl_file           !Output/input file
