9b183e59a78ab1758089f2386e919ccbf70c8630
davidt <davidt@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-06-14 14:06:16 +0000

    Update to working folder in post proc
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@522 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- MD_dCSE/src_code/post_proc/python/work/DT/streamplot.py
+++ MD_dCSE/src_code/post_proc/python/work/DT/streamplot.py
@@ -2,7 +2,17 @@ import matplotlib.pyplot as plt
 from MDPlotData import *
 
 Getter = MD_PlotData('../../results/',cpol_bins=True)
+fig = plt.figure(figsize=(3,12))
+ax = fig.add_subplot(111)
 
-r, z, vr, vz = Getter.get_vplane_streamplot_args(1,0,2,0,160)
-plt.streamplot(r,z,vr,vz)
-plt.show()
+drec = 250
+cnt = 0
+for rec in range(0,250,drec):
+
+	r, z, vr, vz = Getter.get_vplane_streamplot_args(1,0,2,rec,rec+drec)
+	ax.streamplot(r,z,vr,vz)
+
+	plt.savefig('streamplot.'+'%05d'%cnt+'.jpg',bbox_inches='tight')
+	ax.cla()
+
+	cnt += 1
--- MD_dCSE/src_code/post_proc/python/work/DT/vcontours.py
+++ MD_dCSE/src_code/post_proc/python/work/DT/vcontours.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from MDPlotData import MD_PlotData
 
-Getter = MD_PlotData('../../results/')
-T, Z, VR = Getter.get_vplane_splot_args(0,1,2,0,0,200)
+Getter = MD_PlotData('../../results/',cpol_bins=True)
+T, Z, VR = Getter.get_vplane_splot_args(0,1,2,0,0,160)
 plt.contourf(T,Z,VR)
 plt.show()
