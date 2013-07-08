17c3620e9d43cdad0f570853d3949ee8de0fb7f8
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-07-08 15:16:34 +0000

    MATLAB routine updated -- improved read_header to make it more robust. Added 3/4 slice to nozzle plotting
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@536 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/post_proc/MATLAB/read/read_header.m
+++ src_code/post_proc/MATLAB/read/read_header.m
@@ -8,21 +8,41 @@ if (exist('resultfile_dir') == 0)
     display('setting results file to default "./../../results"');
 end
 
-%Import date from header file
-cd(resultfile_dir);
-simulation_header = importdata('./simulation_header',';');
-cd (pwdir);
+% %Import date from header file
+% cd(resultfile_dir);
+% simulation_header = importdata('./simulation_header',';');
+% cd (pwdir);
+% 
+% %Loop through all elements and extract variables
+% for i = 1:size(simulation_header.data,1)
+%     varname =  simulation_header.textdata{i,2};
+%     if (isnan(simulation_header.data(i)) == 0)
+%         evalc([varname '= simulation_header.data(i)']);
+%     else
+%         %disp(['Warning: header variable <', varname, '> is corrupted or not a number. Skipping']);
+%         %Skip this variable (may be a result of character in
+%     end
+% end
 
-%Loop through all elements and extract variables
-for i = 1:size(simulation_header.data,1)
-    varname =  simulation_header.textdata{i,2};
-    if (isnan(simulation_header.data(i)) == 0)
-        evalc([varname '= simulation_header.data(i)']);
+%More robust solution using mixed csv function
+% %Import date from header file
+cd(resultfile_dir);
+simulation_header =read_mixed_csv('./simulation_header',';');
+cd (pwdir)
+
+% %Loop through all elements and extract variables
+for i = 1:size(simulation_header,1)
+	varname = strtrim(simulation_header{i,2});
+    %Check for strings and save accordingly
+	if (isempty(str2num(strtrim(simulation_header{i,3}))) == 0)
+        evalc([varname '= str2num(strtrim(simulation_header{i,3}))']);
     else
+        evalc([varname '= strtrim(simulation_header{i,3})']);
         %disp(['Warning: header variable <', varname, '> is corrupted or not a number. Skipping']);
         %Skip this variable (may be a result of character in
-    end
+	end
 end
+
 %If simulation is not finished, set number of steps to last
 %output step of the simulation
 cd(resultfile_dir); 
@@ -35,3 +55,4 @@ cd (pwdir);
 if velocity_outflag ~= 0
     mass_outflag = velocity_outflag;
 end
+
