b6f8e0987bdbad6c8e27da38b8db565acaac9cd5
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-09-11 15:05:38 +0000

    Forgot to turn off external force in previous committ
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/MD_dCSE@574 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- src_code/control_volume.f90
+++ src_code/control_volume.f90
@@ -35,7 +35,7 @@ type :: check_CV_momentum
 end type check_CV_momentum
 
 	!Check CV conservation
-	logical	:: CV_debug=.true.
+	logical	:: CV_debug=.false.
 	type(check_CV_mass)		:: CVcheck_mass		! declare an instance of CV checker
 	type(check_CV_momentum)	:: CVcheck_momentum, CVcheck_momentum2		! declare an instance of CV checker
 
