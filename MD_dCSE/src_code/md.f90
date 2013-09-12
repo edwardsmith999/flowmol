009945b4e3448a2c1bf795d1a6645e57d73fcfd5
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-09-12 17:28:18 +0000

    Set  CFD_rhouu_dS to zero in continuum force as it introduces noise. Also removed some redundent variables
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@580 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- MD_dCSE/src_code/md.f90
+++ MD_dCSE/src_code/md.f90
@@ -118,7 +118,7 @@ contains
 		call average_and_send_MD_to_CFD(iter)  ! MD=>CFD Calculate averages of MD to pass to CFD
 #else
 		!call apply_flekkoy_test
-		!call apply_CV_force(iter)
+		call apply_CV_force(iter)
 		call apply_boundary_force
 #endif
 
