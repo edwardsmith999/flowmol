7293618f17a7121dea68f32b14031af85db7219d
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2013-10-04 16:12:01 +0000

    Added InputClass.py module to utils which defines dictonaries of lists or lists of dictonaries to be used in the input for batch scripting. These can be added and multiplied in order to very cleanly and efficiectly generate a range of required cases
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@605 1e92d776-e0e5-45cd-b670-8958d04d46c8

---
+++ utils/InputClass.py
@@ -0,0 +1,112 @@
+import string
+
+#List of Dictonary classes with added routines to add and multiple inputs
+class InputList(list):
+    def __init__(self,*arg,**kw):
+        super(InputList, self).__init__(*arg, **kw)
+
+        self.valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+
+    #Define Addition operation as elementwise addition
+    def __add__(self, x):
+
+        if (type(x) == InputDict):
+            Listx = x.expand()
+        elif (type(x) == InputList):
+            Listx = x
+        else:
+            raise TypeError("Unsupported type " + str(type(x))  + " for input addition" + 
+                            " -- must be InputList or InputDict type")
+
+        returnlist = InputList()
+        ziplist = zip(self,Listx)
+        for entry in ziplist:
+            tempdict = {}
+            for dic in entry:
+                tempdict.update(dic)
+            returnlist.append(tempdict)
+
+        return returnlist
+
+    __radd__=__add__
+
+    #Define multiplication operation as all permutations of inputs
+    def __mul__(self,x):
+
+        if (type(x) == InputDict):
+            Listx = x.expand()  
+        elif (type(x) == InputList):
+            Listx = x
+        else:
+            raise TypeError("Unsupported type " + str(type(x))  + " for input multiplication" + 
+                            " -- must be InputList or InputDict type")
+
+        returnlist = InputList()
+        for entry1 in self:
+            for entry2 in Listx:
+                newdict = {}
+                newdict.update(entry1)
+                newdict.update(entry2)
+                returnlist.append(newdict)
+
+        return returnlist
+
+    __rmul__=__mul__
+
+    #Define Addition operation as elementwise addition
+    def zip_inputs(self, x):
+
+        returnlist = self + x
+
+        return returnlist
+
+    #Generate all permutations of inputs and return filenames
+    def outer_product_inputs(self,x):
+
+        returnlist = self*x
+
+        return returnlist
+
+    def filenames(self):
+
+        #Generate list containing filenames
+        filenames=[(''.join(c for c in str(name.items()) 
+                          if c in self.valid_chars[6:]))
+                          for name in self]
+         
+        return filenames
+
+#Dictonary class with added routines to add and multiple inputs
+class InputDict(dict):
+    def __init__(self,*arg,**kw):
+        super(InputDict, self).__init__(*arg, **kw)
+
+    #Expand Dictonary with multiple values per entry into many dictonary elements
+    def expand(self):
+
+        expansion = self.values()[0]
+        returnlist = InputList({self.keys()[0]:e} for e in expansion)
+        
+        return returnlist       
+
+    #Define Addition operation as elementwise addition
+    def __add__(self, x):
+
+        # Convert to lists
+        templist = self.expand()
+        returnlist = templist + x
+
+        return returnlist
+
+    __radd__=__add__
+
+    #Define multiplication operation as all permutations of inputs
+    def __mul__(self,x):
+
+        # Convert to lists
+        templist = self.expand()
+        returnlist = templist * x
+
+        return returnlist
+
+    __rmul__=__mul__
