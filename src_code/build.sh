aae1aafc0691127be796a7d0d9981903f227d22c
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2014-01-30 11:51:37 +0000

    Added build.sh bash script to coupler_src_code to assist in making coupler
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch/coupler_dCSE@644 1e92d776-e0e5-45cd-b670-8958d04d46c8

---
+++ src_code/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+BUILDtype=debug
+if [ $# -lt 1 ]; then
+	cd ./../../;  make BUILD=$BUILDtype couette_md  ;cd ./coupler_dCSE/src_code/;
+else
+	cd ./../../; make clean_all;  make BUILD=$BUILDtype couette_md;cd ./coupler_dCSE/src_code/;
+fi
