798b2aef188bacfe873e0dc48ad01b6fc7ec2a38
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2014-03-03 15:00:46 +0000

    vmd_reformat made into a class with default run as before. Added fortran routine finalstate2vmdout.f90 to reorder a specified final_state file into a vmd output file for debugging. By editing the source  code the range of value can be limited so only a subset of the available molecules are displayed
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@652 1e92d776-e0e5-45cd-b670-8958d04d46c8

--- MD_dCSE/src_code/debug_scripts/vmd_reformat.py
+++ MD_dCSE/src_code/debug_scripts/vmd_reformat.py
@@ -7,23 +7,33 @@
 
 import os
 
-filepath = './../results/'
-headerfile = filepath + 'simulation_header'
-# Extract np from header
-fobj = open(headerfile,'r')
-np = 0
-while np==0:
-	line = fobj.readline().split(';')
-	if (line[1].strip() == 'globalnp'):
-		np   = int(line[2].strip())
-	
-# Either call VMD_reformat with np
-os.system('ifort -O3 -o vmd_reformat.exe vmd_reformat.f90')
-cmd = './vmd_reformat.exe ' + str(np)
-os.system(cmd)
+class VmdReformat:
 
-try:
-	with open('./' + filepath + '/vmd_out.dcd'): pass
-	os.system('vmd ' + filepath + '/vmd_out.dcd')
-except IOError:
-	print 'vmd_out.dcd file missing, reorder unsuccessful'
+    def __init__(self,fdir):
+
+        headerfile = fdir + 'simulation_header'
+        # Extract np from header
+        fobj = open(headerfile,'r')
+        np = 0
+        while np==0:
+            line = fobj.readline().split(';')
+            if (line[1].strip() == 'globalnp'):
+                np   = int(line[2].strip())
+            
+        # Build and call VMD_reformat with np from header
+        os.system('ifort -O3 -o vmd_reformat.exe vmd_reformat.f90')
+        cmd = './vmd_reformat.exe ' + str(np)
+        os.system(cmd)
+
+if __name__ == "__main__": 
+
+    filepath = './../results/'
+
+    VMDobj = VmdReformat(filepath)
+
+    try:
+        with open('./' + filepath + '/vmd_out.dcd'): pass
+        os.system('vmd ' + filepath + '/vmd_out.dcd')
+    except IOError:
+        print 'vmd_out.dcd file missing, reorder unsuccessful'
+
