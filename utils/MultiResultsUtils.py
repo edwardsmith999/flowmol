ec692e46855edf732e189c8c8390a02a709f5ca2
edwards <edwards@1e92d776-e0e5-45cd-b670-8958d04d46c8>
2014-05-27 18:02:31 +0000

    Added MultiResultsUtils.py to combine multiple files from various runs into a single directory
    git-svn-id: http://svn.ma.ic.ac.uk/subversion/edward/MDNS_repo/branch@696 1e92d776-e0e5-45cd-b670-8958d04d46c8

---
+++ utils/MultiResultsUtils.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env/ python
+import os
+import re
+import sys
+import shutil as sh
+import numpy as np
+
+from ResultsUtils import *
+from MDFields import *
+from HeaderData import *
+from MD_PostProc import MD_PostProc
+
+fdir_list = []
+results_dir_base = '/home/es205/scratch/Re400/'
+
+# BINS 64 x 256 x 64
+#fdir_list.append(results_dir_base+'iter0_to_90870/')
+fdir_list.append(results_dir_base+'iter90870_to_390870/')
+fdir_list.append(results_dir_base+'iter390870_to_667054/')
+fdir_list.append(results_dir_base+'iter667054_to_967054/')
+fdir_list.append(results_dir_base+'iter967054_to_1267054/')
+fdir_list.append(results_dir_base+'iter1267054_to_1567054/')
+
+# BINS 84 x 198 x 50 with CV stats included (at eratic intervals in time!?)
+fdir_list.append(results_dir_base+'iter1567054_to_1918000/')
+fdir_list.append(results_dir_base+'iter1918000_to_2233899/')
+fdir_list.append(results_dir_base+'iter2233899_to_2500000/')
+fdir_list.append(results_dir_base+'iter2500000_to_2800000/')
+fdir_list.append(results_dir_base+'iter2800000_to_3100005/')
+fdir_list.append(results_dir_base+'iter3100005_to_3734274/')
+fdir_list.append(results_dir_base+'iter3734274_to_4384274/')
+fdir_list.append(results_dir_base+'iter4384274_to_5000000/')
+fdir_list.append(results_dir_base+'iter5000000_to_5600000/')
+
+outdir = '/home/es205/scratch/Re400/COMBINED_iter0_to_5600000/'
+RU = ResultsUtils(outdir)
+finalrec = 56
+for fdir in fdir_list:
+    fielddict = MD_PostProc(fdir)
+    #get initialrec for each folder (With first runs at high resolution)
+    #initialrec = int(finalrec)
+    for keys, field in fielddict.plotlist.items():
+        if keys in ['mbins','vbins','Tbins']:
+            #Get filename and recordsize
+            filename = fdir + keys
+            if field.Raw.dtype == 'i':
+                typesize = 4
+            elif field.Raw.dtype == 'd':
+                typesize = 8
+            else:
+                quit("Error - datatype not recognised")
+            recbytes = (typesize*field.Raw.nperbin*np.product(field.Raw.nbins))
+            if ((field.Raw.get_maxrec()+1)*recbytes != os.path.getsize(filename)):
+                print(filename,(field.Raw.get_maxrec()+1)*recbytes != os.path.getsize(filename))
+                quit("File sizes do not match")
+
+            #Get starting and final record of data in folder
+            initialstep = int(field.Raw.header.initialstep)
+            with open(fdir+'simulation_progress', 'r') as f:
+                finalrec_progress = int(f.readline())-initialstep
+                if ( float(field.Raw.header.Nsteps)
+                    -float(finalrec_progress)
+                    >float(field.Raw.header.tplot)):
+                    finalstep = int(finalrec_progress)
+                    finished = False
+                    print("Simulation not finished! = ", finalstep)
+                elif ((int(finalrec_progress) == int(field.Raw.header.Nsteps)) |
+                     ( float(field.Raw.header.Nsteps)
+                      -float(finalrec_progress)
+                      <float(field.Raw.header.tplot))):
+                    finalstep = int(field.Raw.header.Nsteps)
+                    finished = True
+                    print("Simulation finished = ", finalstep)
+                else:
+                    quit("Error -- simulation progress thinks run is longer than specified number of Nsteps!!!")
+
+            plotfreq = float(field.plotfreq) * float(field.Raw.header.tplot)
+
+            initialrec = np.floor( float(initialstep)/float(plotfreq) )
+            finalrec = initialrec + np.floor( float(finalstep)/float(plotfreq) )
+            #print(finished,"Writing files in ", filename, "with starting recno = ",int(initialrec), "To final recno = ",int(initialrec)+os.path.getsize(filename)/recbytes, "into folder = ", RU.outdir)
+
+            #print(filename,recbytes,initialstep,finalstep,int(field.Raw.header.tplot),finalrec,int(initialrec)+os.path.getsize(filename)/recbytes,RU.outdir)
+
+           
+
+            #RU.DismemberResults(fdir, recbytes,initialrec=finalrec)
+    
+
+
+
