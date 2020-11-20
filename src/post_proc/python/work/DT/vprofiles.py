91d21bd2448fdd5883200ca979361f0ec0ab5294
Ed <edward.smith0@brunel.ac.uk>
2020-11-20 10:07:45 +0000

    Cleaned up a load of old files and fixed path for build system

--- src/post_proc/python/work/DT/vprofiles.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-from MDPlotData import *
-
-Getter = MD_PlotData('../../results/',cpol_bins=True)
-r, vprofile = Getter.get_vslice_plot_args(0,0,160)
-plt.plot(vprofile,r,'o')
-plt.show()
